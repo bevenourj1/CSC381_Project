c9135c6f62c80bd53252058346ac408bb5279dd3
bevenourj1 <117945280+bevenourj1@users.noreply.github.com>
2023-10-19 11:54:56 -0400

    CSV Reader Version 2
    Accurate displays all rows and columns of csv file, along with distinguishing headers. Can scroll through data using horizontal and vertical scrollbar.

--- CSVReader.py
+++ CSVReader.py
@@ -1,59 +1,83 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
 import os
+import csv
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-root = tk.Tk()
-root.geometry("400x400")   # size of interface width x height
-root.pack_propagate(0)
+colnames = []
+class CSVReader(tk.Frame):
+     def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        
+        
+        def UploadAction(event=None):
+                delete_command()
+                global colnames
+                colnames = []
+                filename = filedialog.askopenfilename()
+                # Open and read the CSV file
+                with open(filename, "r") as csv_file:
+                    csv_reader = csv.reader(csv_file)
+                    
+                    for row in csv_reader:
+                        for element in row:
+                            colnames.append(element)
+                        self.tree.configure(columns=colnames)    
+                        break
+
+                    for colindex, col in enumerate(colnames):
+                        self.tree.heading("#" + str(colindex + 1), text=col)   
+
+                    for row in csv_reader:
+                        self.tree.insert("", "end", values=row)
+        
+        def delete_command():
+            for col in self.tree['columns']:
+                self.tree.heading(col, text='')
+            self.tree.delete(*self.tree.get_children())
+
+        self.topFrame = tk.Frame(self, bg="darkred", pady="10", padx="10", 
+                            width="200", height="50")     
+        self.topFrame.pack(side=tk.TOP)   
+        self.topFrame.pack_propagate(0)
 
 
-def UploadAction(event=None):
-    descCols = []
-    col_names = []
-    filename = filedialog.askopenfilename()
-    with open(filename, "r") as f:
-        for line in f:
-            if line.strip():
-                x = line.rstrip().split(",") 
-                descCols.append(x)
-                break
-    for cols in descCols:
-        for names in cols:
-            col_names.append(names)
-    for i, col_names in enumerate(col_names, start=1):
-        tk.Label(bottomFrame, text = col_names).grid(row=3, column=i, padx=10)
-   
+        
+        self.headerLabel = tk.Label(self.topFrame, text=" Upload CSV ", 
+                            bg="white", fg="navy", font="24")
+        self.headerLabel.pack(fill=tk.X)      
 
 
+        self.middleFrame = tk.Frame(self, bg="white", pady="10", padx="10", 
+                            width="200", height="100")
 
-# establish three main vertical regions
-topFrame = tk.Frame(root, bg="darkred", pady="10", padx="10", 
-                    width="400", height="100")     # frame is a container, root is its parent
-topFrame.pack(side=tk.TOP)    # pack() helps determine where an item will be placed
-topFrame.pack_propagate(0)
-# without pack_propagate(0) tkinter sizes things to just the size needed
-# with pack_propagate(0) you must set a width and height or they are by default 0
+        self.middleFrame.pack()
+        self.middleFrame.pack_propagate(0)
 
-middleFrame = tk.Frame(root, bg="white", pady="10", padx="10", 
-                       width="400", height="300")
-middleFrame.pack()
-middleFrame.pack_propagate(0)
+        self.button = tk.Button(self.middleFrame, text='Open', command=UploadAction)
+        self.button.pack()
+
+        self.bottomFrame = ttk.Frame(self)
+        self.bottomFrame.pack(expand=tk.YES, fill=tk.BOTH)
 
 
-# https://www.tutorialspoint.com/python/tk_label.htm
-headerLabel = tk.Label(topFrame, text=" Upload CSV ", 
-                       bg="white", fg="navy", font="24")
-# bg background color  fg foreground/font color 
-headerLabel.pack(fill=tk.X)      # fill container horizontally
-# https://www.tutorialspoint.com/python/tk_pack.htm
+        self.tree = ttk.Treeview(self.bottomFrame,columns=colnames, show='headings')
+
+        self.scrollbar = ttk.Scrollbar(self.tree, orient="vertical", command=self.tree.yview)
+        self.scrollbar2 = ttk.Scrollbar(self.tree, orient="horizontal", command=self.tree.xview)
+        self.tree.configure(yscrollcommand= self.scrollbar.set)
+        self.tree.configure(xscrollcommand= self.scrollbar2.set)
+
+                # Pack the Treeview and scrollbar
+        self.tree.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
+        self.scrollbar2.pack(side="bottom", fill="x")
 
 
-button = tk.Button(middleFrame, text='Open', command=UploadAction)
-button.pack()
-
-bottomFrame = tk.Frame(root, bg="navy", pady="10", padx="10", 
-                       width="400", height="225")
-bottomFrame.pack()
-bottomFrame.pack_propagate(0)
-
-root.mainloop()
+if __name__ == "__main__":
+    root=tk.Tk()
+    root.geometry("1500x500")
+    root.pack_propagate(0)
+    CSVReader(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
