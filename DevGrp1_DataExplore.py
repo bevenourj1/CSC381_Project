a4ce2a5adb2c787ce35006b8c079e1ff86655e10
bevenourj1 <117945280+bevenourj1@users.noreply.github.com>
2023-11-09 14:19:08 -0500

    DevGrp1_DataExplore (With Col Numbers)
    Most recent version with column numbers

--- DevGrp1_DataExplore.py
+++ DevGrp1_DataExplore.py
@@ -43,7 +43,7 @@ class CSVReader(tk.Frame):
         def reset():
             self.tree.delete(*self.tree.get_children())
             for colindex, col in enumerate(colnames):
-                self.tree.heading("#" + str(colindex + 1), text=col)   
+                self.tree.heading("#" + str(colindex + 1), text=str(colindex) + str('. ') + col)   
             for row in data:
                 if row == colnames:
                     pass
@@ -255,7 +255,7 @@ class CSVReader(tk.Frame):
                         col_outputs = {colname: [] for colname in colnames}
 
                     for colindex, col in enumerate(colnames):
-                        self.tree.heading("#" + str(colindex + 1), text=col)   
+                        self.tree.heading("#" + str(colindex + 1), text=str(colindex) + str('. ') + col)  
 
 
                     for row in csv_reader:
